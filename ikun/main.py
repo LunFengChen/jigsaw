05e7cb5544ae29e062d36b225f1c12459ae923d1
XiaoFeng <2292990278@qq.com>
2022-10-15 21:58:00 +0800

    v2

--- ikun/main.py
+++ ikun/main.py
@@ -5,7 +5,7 @@
 
 import requests
 
-
+"https://cn.bing.com/images/search?q=%E8%94%A1%E5%BE%90%E5%9D%A4%E5%9B%BE%E7%89%87&go=%E6%90%9C%E7%B4%A2&qs=ds&form=QBIR&first=1&tsc=ImageHoverTitle"
 url = "https://cn.bing.com/images/search?q=%E8%94%A1%E5%BE%90%E5%9D%A4%E5%9B%BE%E7%89%87&go=%E6%90%9C%E7%B4%A2&qs=ds&form=QBIR&first=1&tsc=ImageHoverTitle"
 
 headers = {
